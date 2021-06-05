ae875a0e5147487967b2a046084c6b703aacfe43
DaeYoung Nam <wsnam0507@naver.com>
2021-06-06 02:27:44 +0900

    greedy

--- src/com/algo/greedy/A1931.java
+++ src/com/algo/greedy/A1931.java
@@ -3,9 +3,7 @@ package com.algo.greedy;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class A1931 {
 
@@ -20,15 +18,27 @@ public class A1931 {
                     Integer.parseInt(tokenizer.nextToken()) };
             meetingList.add(I);
         }
+        Collections.sort(meetingList, (int[] o1, int[] o2) -> {
+            if(o1[1] == o2[1]) {
+                return o1[0] - o2[0];
+            }
 
-        for(int i = 0; i < meetingList.size(); i++) {
+            return o1[1] - o2[1];
+        });
+
+        int before = meetingList.get(0)[1];
+        int cnt = 1;
+        int size = meetingList.size();
+        for(int i = 1; i < size; i++) {
             int[] meet = meetingList.get(i);
 
-            for(int j = i + 1; j < meetingList.size(); j++) {
-                int[] target = meetingList.get(j);
-
+            if(before <= meet[0]) {
+                cnt++;
+                before = meet[1];
             }
         }
+
+        System.out.println(cnt);
     }
 
 }
