4b7bc38d8c0c1d5715119d740264d2d89c3ae0b7
DaeYoung Nam <wsnam0507@naver.com>
2021-06-08 00:10:06 +0900

    greedy

---
+++ src/com/algo/greedy/A13305.java
@@ -0,0 +1,12 @@
+package com.algo.greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class A13305 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
--- src/com/algo/greedy/A2141.java
+++ src/com/algo/greedy/A2141.java
@@ -3,23 +3,47 @@ package com.algo.greedy;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class A2141 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(reader.readLine());
-        int[] X = new int[T];
-        int[] A = new int[T];
+        List<long[]> list = new ArrayList<>();
+        long total = 0;
 
         for(int i = 0; i < T; i++) {
             StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
-            X[i] = Integer.parseInt(tokenizer.nextToken());
-            A[i] = Integer.parseInt(tokenizer.nextToken());
+            long X = Long.parseLong(tokenizer.nextToken());
+            long A = Long.parseLong(tokenizer.nextToken());
+            list.add(new long[] { X, A });
+            total += A;
         }
+        Collections.sort(list, new Comparator<long[]>() {
+            @Override
+            public int compare(long[] o1, long[] o2) {
+                if(o1[0] == o2[0]) {
+                    return (int) (o1[1] - o2[1]);
+                }
 
+                return (int) (o1[0] - o2[0]);
+            }
+        });
 
+        total += 1;
+        total /= 2;
+        
+        long answer = 0;
+        for(int i = 0; i < T; i++) {
+            long[] l = list.get(i);
+            answer += l[1];
+
+            if(answer >= (total + 1) / 2) {
+                System.out.println(String.valueOf(l[0]));
+                return;
+            }
+        }
     }
 
 }
